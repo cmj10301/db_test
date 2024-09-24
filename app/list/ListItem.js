5b37a2ebddbb60e7bac3dec1e47abd31893ba3e8
cmj10301 <cmj10301@gmail.com>
2024-09-24 10:26:28 +0900

    삭제기능 구현

--- app/list/ListItem.js
+++ app/list/ListItem.js
@@ -3,21 +3,20 @@
 import Link from "next/link"
 
 export default function ListItem({result}) {
+    
     return (
     <div className="list-bg">
         {
             JSON.parse(result).map((a, i) => {
+                console.log(a._id)
                 return (
                     <div className="list-item" key={i}>
-                        
-                        console.log(result)
-                        <h4>{a._id.toString()}</h4>
-                        <Link href={'/detail/' + result[i]._id}>
-                          <h4>{result[i].title}</h4>
+                        <Link href={'/detail/' + a._id}>
+                          <h4>{a.title}</h4>
                         </Link>
-                        <Link href={'/edit/' + result[i]._id}>수정</Link>
+                        <Link href={'/edit/' + a._id}>수정</Link>
                         <span onClick={() =>
-                            fetch('/api/post/delete', {method : 'DELETE', body : result[i]._id})
+                            fetch('/api/post/delete', {method : 'DELETE', body : a._id})
                         }>삭제</span>
                         <p>1월 1일</p>
                     </div>
